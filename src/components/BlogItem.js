99c4587580a3d3a00daea3e3f1957f0186c42b9a
Ar3Tea <tbusinessr@gmail.com>
2019-01-09 16:15:52 -0500

    updating

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -4,7 +4,7 @@ import BlogContent from '../components/BlogContent';
 
 const BlogItem = (props) => (
   <div className="box">
-    <BlogContent limit={150} {...props }>
+    <BlogContent limit={50} {...props }>
       <Link className="level-item button is-small is-link is-outlined" to={{pathname: `/blog${props.path}`, state: { props } }}>Read More</Link>
       {/*<BlogNav date={props.date} to={{pathname: `/blog${props.path}`, state: { props } }} /> */}
     </BlogContent>
--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 const PageContent = styled.div`
   margin: 0 auto;
-  max-width: 960px;
+  max-width: 90%;
   padding: 1.5rem;
 `
 export default PageContent
